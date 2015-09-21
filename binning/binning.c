8db7f653635876becf57bceb291b959271622f12
Yuanyuan Zhang <yzhang171@dons.usfca.edu>
2015-09-21 00:15:54 -0700

    add basic ideas for binning

--- binning/binning.c
+++ binning/binning.c
@@ -23,11 +23,61 @@ SOFTWARE.
 */
 
 #include "binning.h"
+#include <morecore.h>
+#include <stddef.h>
+
+void * heap; // each time request a fixed max size memory 4096 byte
+
+/* ten fixed size bin, each bin contains a free list, max size for malloc
+* is 4096 bytes, for each malloc request, it will start from bin 0 to find
+* the best fit size, return the index of bin,then return address.
+* If there is no free block in bin[index], continue search next index(index++),
+* till find one (index <=9),for loop to divide the free block by 2 till the
+* size best fit the request.at the same time add the rest unused free block to
+* each bin against size.
+* if index > 9, request new heap, repeat divide free block and add free block
+* to free list, and then return address finally.
+*/
+Free_Header * bin[10];
+/**** SIZES FOR EACH BIN ****
+ * bin[0] -> 8
+ * bin[1] -> 16
+ * bin[2] -> 32
+ * bin[3] -> 64
+ * bin[4] -> 128
+ * bin[5] -> 256
+ * bin[6] -> 512
+ * bin[7] -> 1024
+ * bin[8] -> 2048
+ * bin[9] -> 4096
+ */
+
+void heap_init(){
+
+	heap = morecore(DEFAULT_MAX_HEAP_SIZE);
+	bin[9] = (Free_Header *) heap;
+	bin[9]->free = 1;
+	bin[9]->size = DEFAULT_MAX_HEAP_SIZE;
+	bin[9]->next = NULL;
+	bin[9]->prev = NULL;
+}
 
 void *malloc(size_t size) {
+	size_t actual_size = request2size(size);
+	if(actual_size > DEFAULT_MAX_HEAP_SIZE) return NULL;
+}
 
+void *calloc(size_t n, size_t size){
 }
 
 void free(void *ptr) {
+
+}
+
+int get_bin_index(size_t){
+
+}
+
+Free_Header* find_free_buddy(Free_Header*){
 
 }
--- binning/binning.h
+++ binning/binning.h
@@ -22,12 +22,45 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#ifndef MALLOC_MERGING_H
-#define MALLOC_MERGING_H
+#ifndef MALLOC_BINNING_H
+#define MALLOC_BINNING_H
 
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "morecore.h"
 
+typedef struct _Free_Header {
+	uint16_t free; // 1 means free , 0 means in use
+	uint16_t size;
+	struct _Free_Header *next;
+	struct _Free_Header *prev;
+} Free_Header;
+
+static const size_t MIN_CHUNK_SIZE = sizeof(Free_Header);
+
+static const size_t WORD_SIZE_IN_BYTES = sizeof(void *);
+
+static const size_t ALIGN_MASK = WORD_SIZE_IN_BYTES - 1;
+
+static const size_t DEFAULT_MAX_HEAP_SIZE = 4096;
+
+static inline size_t size_with_header(size_t n) {
+	return n + sizeof(Free_Header) <= MIN_CHUNK_SIZE ? MIN_CHUNK_SIZE : n + sizeof(Free_Header);
+}
+
+static inline size_t align_to_word_boundary(size_t n) {
+	return (n & ALIGN_MASK) == 0 ? n : (n + WORD_SIZE_IN_BYTES) & ~ALIGN_MASK;
+}
+
+static inline size_t request2size(size_t n) {
+	return align_to_word_boundary(size_with_header(n));
+}
+
+void heap_init();
 void *malloc(size_t);
+void *calloc(size_t n, size_t size);
 void free(void *);
+int get_bin_index(size_t);
+Free_Header* find_free_buddy(Free_Header*); //merge neighbour free blocks
 
-#endif //MALLOC_MERGING_H
+#endif //MALLOC_BINNING_H
